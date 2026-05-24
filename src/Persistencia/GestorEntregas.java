2a2af3f421675d595e516432c0c4e19eca3af7c9
Sergio-57832 <sergioyesidtorresarias@gmail.com>
2026-05-24 17:04:54 -0500

    Gestor de entregas terminado

--- src/Persistencia/GestorEntregas.java
+++ src/Persistencia/GestorEntregas.java
@@ -1,5 +1,46 @@
 package Persistencia;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.ArrayList;
+import Modelo.Entrega;
 
 public class GestorEntregas {
     
+    private static final String rutaDatos = "Datos/entregas.txt";
+    
+    public void guardarEntregas(Entrega entrega){
+    
+        try(BufferedWriter escritor = new BufferedWriter(new FileWriter(rutaDatos,true))){
+            escritor.write(entrega.getId() + ";"
+                    + entrega.getIdPedido() + ";"
+                    + entrega.getFecha() + ";"
+                    + entrega.getHora()+ ";"
+                    + entrega.getValorParcial());
+            escritor.newLine();
+        }catch(IOException error){
+            System.out.println("Error al guardar entrega/s: " + error.getMessage());
+        }
+    }
+    public List<Entrega> cargarEntregas(){
+        List<Entrega> listaEntregas = new ArrayList<>();
+        try(BufferedReader lector = new BufferedReader(new FileReader(rutaDatos))){
+            String renglon;
+            while((renglon = lector.readLine()) != null){
+                String[] atributosEntrega = renglon.split(";");
+                Entrega entrega = new Entrega(atributosEntrega[0],
+                                                atributosEntrega[1],
+                                                atributosEntrega[2],
+                                                atributosEntrega[3],
+                                                Double.parseDouble(atributosEntrega[4]));
+                listaEntregas.add(entrega);
+            }
+        }catch(IOException error){
+            System.out.println("Error al cargar entregas: " + error.getMessage());
+        }
+        return listaEntregas;
+    }
 }
