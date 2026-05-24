050219346d6c57b7d5bc692ec9cddfe8fe898975
Sergio-57832 <sergioyesidtorresarias@gmail.com>
2026-05-24 10:29:58 -0500

    Puse el proyecto por default

--- src/proyecto/poo/ProyectoPOO.java
+++ src/proyecto/poo/ProyectoPOO.java
@@ -4,30 +4,7 @@ public class ProyectoPOO {
 
     public static void main(String[] args) {
         
-        System.out.println("yoan equisde");
-        System.out.println("sergio es uwu");
-        System.out.println("thankyou sergito");
-
-        System.out.println("yoan mini");
-        System.out.println("jjj");
-
         
-        System.out.println("Gay el que lo lea");
-
-        System.out.println("Gay");
-        
-        
-        System.out.println("hola equisde");
-        
-        System.out.println("yoan");
-        
-        System.out.println("xd");
-        
-        System.out.println("ola");
-        
-        System.out.println("HOLA");
-        
-        System.out.println("AJA");
         
     }
     
