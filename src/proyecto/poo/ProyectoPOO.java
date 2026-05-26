a2b8f453db2ceece07c5edc227d77da6e26c5b3e
samuelandreshaz-wq <samuelandreshaz@gmail.com>
2026-05-25 22:22:06 -0600

    Agregue una Interfaz FlatLaf

--- src/proyecto/poo/ProyectoPOO.java
+++ src/proyecto/poo/ProyectoPOO.java
@@ -1,8 +1,10 @@
 package proyecto.poo;
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class ProyectoPOO {
 
     public static void main(String[] args) {
+        FlatLightLaf.setup();
         new Vista.MenuPrincipalVista().setVisible(true);
         
         
